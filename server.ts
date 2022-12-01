a1a605aae3ffc5c9c0345c9d17ee760f83f5763f
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-12-01 10:38:59 +0100

    install vercel1

--- server.ts
+++ server.ts
@@ -15,11 +15,11 @@ export default async(app:Express) => {
 
   app.use(router);
   app.use(express.json({ limit: "50mb" }));
-  app.use('/', (_req, res) => {
-    res.send("TapTap Server")
-  })
+  
   routerTaps(app);
-
+  app.use('/', (_req, res) => {
+      res.send("TapTap Server")
+    })
   app.listen(process.env.PORT_SERVER || 4000, () => {
     console.log("Server running");
   })
