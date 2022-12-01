aa1299129184b62c2d2a6fd9071a70dedaee31c9
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-12-01 10:41:44 +0100

    server hello1

--- index.ts
+++ index.ts
@@ -17,9 +17,7 @@ const startServer = async () => {
       app.listen(process.env.PORT || 4000, () => {
         console.log(`Server is running on port ${process.env.PORT}`)
       });
-      app.use('/', (_req, res) => {
-        res.send("TapTap Server")
-      })
+      
     }catch(error){
       console.log("Error connecting to DB")
     }
