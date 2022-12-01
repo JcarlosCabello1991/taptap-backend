6c8f0650e80ddf016f03280fb60e99422f174f88
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-12-01 10:29:48 +0100

    server hello

--- server.ts
+++ server.ts
@@ -15,7 +15,9 @@ export default async(app:Express) => {
 
   app.use(router);
   app.use(express.json({ limit: "50mb" }));
-
+  app.use('/', (_req, res) => {
+    res.send("TapTap Server")
+  })
   routerTaps(app);
 
   app.listen(process.env.PORT_SERVER || 4000, () => {
